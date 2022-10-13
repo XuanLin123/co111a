df28b2ba948ea1aced2cc98d6ed4ed2f59c31eec
XuanLin123 <f131406298@gmail.com>
2022-10-13 21:07:45 +0800

    1013

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,44 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@2          //R2歸0
+M=0
+
+@0
+D=M
+
+@1
+D=M
+
+@3
+M=D
+
+@loop       //取R0值進迴圈(" > 0 ")       
+D;JGT
+
+(loop)
+
+@1
+D=M
+
+@3          //R3值控制要不要跳EXIT
+M=M-1
+D=M
+
+@EXIT
+D;JLT
+
+@0          //加
+D=M         //到
+
+@2          //R2
+M=M+D       //裡
+
+@loop
+0;JMP
+
+(EXIT)
+@EXIT
+0;JMP
+
